41d51501f877ba9fb3a655bd85f3629f49ff4e17
regcostajr <regcostajr@gmail.com>
2022-10-02 07:55:12 -0300

    wip

--- lib/REFECO/Blockchain/SmartContracts/Solidity/ABI/Encoder.pm
+++ lib/REFECO/Blockchain/SmartContracts/Solidity/ABI/Encoder.pm
@@ -38,9 +38,6 @@ Please report any bugs or feature requests to C<bug-refeco-blockchain-smartcontr
 the web interface at L<https://rt.cpan.org/NoAuth/ReportBug.html?Queue=REFECO-Blockchain-SmartContracts-Solidity-ABI-Encoder>.  I will be notified, and then you'll
 automatically be notified of progress on your bug as I make changes.
 
-
-
-
 =head1 SUPPORT
 
 You can find documentation for this module with the perldoc command.
@@ -85,44 +82,116 @@ use Object::Pad;
 use REFECO::Blockchain::SmartContracts::Solidity::ABI::Type;
 
 class Encoder {
+    use Carp;
     use Digest::Keccak qw(keccak_256_hex);
     field @function_params;
-    field $function_name;
+    field $function_name :accessor;
+
+=head2 append
+
+Includes a new instance for the given type signature
+
+=over 4
+
+=item C<%param> containing the key as the type signature and the value as the value
+
+=back
+
+Returns $self
+
+=cut
 
     method append(%param) {
         for my $type_signature (keys %param) {
-            if ($type_signature eq 'name') {
-                $function_name = $param{$type_signature};
-                next;
-            }
             push(@function_params, Type->get_instance($type_signature, $param{$type_signature}));
         }
 
         return $self;
     }
 
+=head2 function
+
+Set the function name
+
+=over 4
+
+=item C<$item> function name
+
+=back
+
+=cut
+
+    method function($name) {
+        $self->function_name($name);
+        return $self;
+    }
+
+=head2 generate_function_signature
+
+Using the given appended values generate the function signature
+
+=over 4
+
+=back
+
+Returns the function signature string
+
+=cut
+
     method generate_function_signature() {
-        my $signature = $function_name . '(';
+        croak "Missing function name" unless $self->function_name;
+        # this method requires the function name to be set
+        my $signature = $self->function_name . '(';
         $signature .= sprintf("%s,", $_->signature) for @function_params;
         chop $signature;
         return $signature . ')';
     }
 
+=head2 encode_function_signature
+
+Encode the function signature using SHA256
+
+=over 4
+
+=back
+
+Return the first 4 bytes from the SHA256 encoded function signature string
+
+=cut
+
     method encode_function_signature() {
         return sprintf("0x%.8s", keccak_256_hex($self->generate_function_signature));
     }
 
+=head2 encode
+
+Encode all appended types in a single ABI encoded string
+
+=over 4
+
+=back
+
+Returns an ABI encoded string
+
+=cut
+
     method encode() {
         my (@static, @dynamic);
-        push @static, $self->encode_function_signature() if $function_name;
+        # if the function name is given includes it to the encoded string
+        push @static, $self->encode_function_signature() if $self->function_name;
+        # the initial offset is important as all the dynamic values will
+        # depend on this info to set their offset
         my $offset = $self->get_initial_position();
         for my $param (@function_params) {
             $param->encode();
+            # for dynamic items the offset must be included
             if ($param->is_dynamic) {
                 push @static,  sprintf("%064s", sprintf("%x", $offset * 32));
                 push @dynamic, $param->encoded_value;
+                # 64 characters = 32 bytes
                 $offset += length($param->encoded_value) / 64;
             } else {
+                # static items require only the encoded value
                 push @static, $param->encoded_value;
             }
         }
@@ -130,13 +199,31 @@ class Encoder {
         return join('', @data);
     }
 
+=head2 get_initial_position
+
+Calculate the initial offset position for the given appended types
+
+The offset here is just the unsigned integer space that the static parameters
+will require, it will need to be set to an 32 bytes hexadecimal to be included
+
+=over 4
+
+=back
+
+Returns the unsigned integer position for the offset start
+
+=cut
+
     method get_initial_position() {
         my $offset = 0;
         for my $param (@function_params) {
+            # if the function is an array with a fixed size, the count
+            # needs to be included in the offset
             if (!$param->is_dynamic && $param->isa('Array')) {
                 $offset += $param->get_array_size_from_signature();
                 next;
             }
+            # if is not an array 1 as default size
             $offset += 1;
         }
 
--- lib/REFECO/Blockchain/SmartContracts/Solidity/ABI/Type.pm
+++ lib/REFECO/Blockchain/SmartContracts/Solidity/ABI/Type.pm
@@ -9,36 +9,103 @@ use Object::Pad;
 my $_package = __PACKAGE__;
 
 role Type {
-    # package required parameters
+    use Carp;
+    # required parameters
     field $signature :param :accessor;
     field $value     :param :accessor;
 
+    # internal control
     field $encoded_value :accessor;
 
+    # external methods
     method encode;
-    method decode;
     method is_dynamic;
+
+=head2 get_fixed_size_from_signature
+
+Filter out the base type size (eg. int256 = 256)
+
+=back
+
+Returns the unsigned integer base type size or undef
+
+=cut
 
     method get_fixed_size_from_signature() {
         $self->signature =~ /^(?:[a-z]+)(\d+)?/;
         return $1;
     }
 
+=head2 get_base_signature
+
+Filter out the base type (eg. int/string/bytes) from the type signature
+
+=back
+
+Returns the base type string or undef
+
+=cut
+
     method get_base_signature() {
         $self->signature =~ /^([a-z]+(\d+)?)/;
         return $1;
     }
 
+=head2 encode_integer
+
+Encode the given unsigned integer to an 32 bytes padded left hexadecimal
+
+=over 4
+
+=item C<$value> unsigned integer to be encoded
+
+=back
+
+Returns 32 bytes padded hexadecimal
+
+=cut
+
     method encode_integer($value) {
         return sprintf("%064s", sprintf("%x", $value));
     }
+
+=head2 encode_offset
+
+Encode the given unsigned integer to an 32 bytes padded left hexadecimal
+
+This function will multiple the value by 32 to fit the ABI line length
+
+=over 4
+
+=item C<$value> unsigned integer to be encoded
+
+=back
+
+Returns 64 bytes padded hexadecimal
+
+=cut
 
     method encode_offset($value) {
         return $self->encode_integer($value * 32);
     }
 
+=head2 get_instance
+
+Based on the give type signature finds the reference module to handle the type encoding
+
+=over 4
+
+=item C<$signature> type signature
+
+=item C<$value> type value
+
+=back
+
+Returns a L<REFECO::Blockchain::SmartContracts::Solidity::ABI::Type> based instance for the given type
+
+=cut
+
     method get_instance :common :($signature, $value) {
-        $DB::single = 1;
         my $package;
         if ($signature =~ /\[(\d+)?\]/gm) {
             $package = "Array";
@@ -51,9 +118,10 @@ role Type {
         } elsif ($signature =~ /^string$/) {
             $package = "String";
         } else {
-            die "Module not found for the given parameter signature";
+            croak "Module not found for the given parameter signature";
         }
 
+        # this is just to avoid `use module` for every new type included
         (my $path = $_package) =~ s|::|/|g;
         my $package_file = sprintf("%s/%s.pm", $path, $package);
         require $package_file;
